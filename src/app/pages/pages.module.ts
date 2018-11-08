b60f48b3ff4d9f6c279ade7e2ac3bd75d6e22efb
Jose Wilder <joswilder46@gmail.com>
2018-11-08 10:41:13 -0200

    mvp atualization

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,16 +4,26 @@ import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { AppRoutingModule } from '../app.routing';
 import { HomepageComponent } from './homepage/homepage.component';
-
-
-
+import { MzInputModule } from 'ngx-materialize'
+import { MzIconModule, MzIconMdiModule } from 'ngx-materialize'
+import { MzSelectModule } from 'ngx-materialize'
+import { MzButtonModule } from 'ngx-materialize';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MzInputModule,
+    MzIconModule,
+    MzIconMdiModule,
+    MzSelectModule,
+    MzButtonModule
+    
+    
   ],
-  declarations: [HomeComponent, SearchComponent, HomepageComponent],
-  exports:[HomeComponent, SearchComponent, HomepageComponent]
+  declarations: [HomeComponent, SearchComponent, HomepageComponent, RegisterComponent, LoginComponent],
+  exports:[HomeComponent, SearchComponent, HomepageComponent,]
 })
 export class PagesModule { }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Fretista } from '../../interfaces/fretistas'
 import { FretistaService } from '../../_services/fretistas.service'
-
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
